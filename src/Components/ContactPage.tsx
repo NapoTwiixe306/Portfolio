a261b9e76cc137ce8b84d461e080783ddc28646d
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-06-10 14:23:42 +0200

    end repsonsive

--- src/Components/ContactPage.tsx
+++ src/Components/ContactPage.tsx
@@ -1,46 +1,71 @@
-import React from 'react'
-import { Footer } from './Widget'
-const ContactPage = () => {
+import React, { FormEvent } from 'react';
+import { Footer } from './Widget';
+
+const ContactPage: React.FC = () => {
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
+
+    // Récupération des valeurs du formulaire
+    const target = event.target as typeof event.target & {
+      name: { value: string };
+      email: { value: string };
+      message: { value: string };
+    };
+
+    const name = target.name.value;
+    const email = target.email.value;
+    const message = target.message.value;
+
+    // Affichage des valeurs en console
+    console.log('Nom :', name);
+    console.log('Email :', email);
+    console.log('Message :', message);
+
+
+    // Vous pouvez ajouter ici votre logique d'envoi du formulaire vers un serveur, par exemple via une requête HTTP
+  }
+
   return (
     <>
-    <div id="contact"/>
-    <div className='contact-container'>
+      <div id="contact" />
+      <div className='contact-container'>
         <h1>Contact</h1>
-        <div className="contact"> 
-            <form  className="contact-form">
-                <div className="form-group">
-                    <label htmlFor="name" className='form-label'>Nom : </label>
-                    <input 
-                        type="text"
-                        id='name'
-                        name="name"
-                        placeholder='Name'
-                        className='form-control'/>
-                </div>
-                <div className="form-group">
-                <label htmlFor="email" className="form-label">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message" className='form-label'>Message : </label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        placeholder="Message">
-                    </textarea>
-                </div>
-                <button type="submit" className="submit-btn">Submit</button>
-            </form>
+        <div className="contact">
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name" className='form-label'>Nom : </label>
+              <input
+                type="text"
+                id='name'
+                name="name"
+                placeholder='Name'
+                className='form-control' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email" className="form-label">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="form-control" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message" className='form-label'>Message : </label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message">
+              </textarea>
+            </div>
+            <button type="submit" className="submit-btn">Submit</button>
+          </form>
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default ContactPage
+export default ContactPage;
--- src/Components/Widget/Footer.tsx
+++ src/Components/Widget/Footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <>
-    <div>
-      <h1>Footer</h1>
-    </div>
+      <footer className="footer">
+            <p className="footer__text">Copyright © 2023 Julien-Milants   |</p>
+            <a href="https://github.com/NapoTwiixe306" target="_blank" rel="noopener noreferrer" className="footer__link footer__link--github">
+                <FontAwesomeIcon icon={faGithub} size="2x" className="footer__icon footer__icon--github" />
+            </a>
+            <a href="https://CryptoExtension.fr" target="_blank" rel="noopener noreferrer" className="footer__link footer__link--google">
+                <FontAwesomeIcon icon={faGoogle} size="2x" className="footer__icon footer__icon--google" />
+            </a>
+            <a href="https://www.instagram.com/julien.mlnts/" target="_blank" rel="noopener noreferrer" className="footer__link footer__link--instagram">
+                <FontAwesomeIcon icon={faInstagram} size="2x" className="footer__icon footer__icon--instagram" />
+            </a>
+        </footer>
     </>
   )
 }
